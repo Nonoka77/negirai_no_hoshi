775d018684c6e40187ba2777a630bf515ccab228
Nonoka77 <ptxdy77@gmail.com>
2021-10-22 16:14:29 +0900

    Fix: routeを指定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'static_pages/top'
-  get 'static_pages/privacy'
-  get 'static_pages/term'
+  root 'static_pages#top'
+  get 'static_pages/top', to: 'static_pages#top'
+  get 'static_pages/privacy', to: 'static_pages#privacy'
+  get 'static_pages/term', to: 'static_pages#term'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
