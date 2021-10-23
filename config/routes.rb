97a7ec9d2b6f46f181da80a212d149e40078bcb1
Nonoka77 <ptxdy77@gmail.com>
2021-10-23 12:55:04 +0900

    Fix: deviceをカスタマイズ

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => {
-    :registrations => 'users/registrations',
-    :sessions => 'users/sessions'   
-  } 
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: "users/sessions",
+  }
 
-  devise_scope :user do
-    get "sign_in", :to => "users/sessions#new"
-    get "sign_out", :to => "users/sessions#destroy" 
-    
   root 'static_pages#top'
   get 'static_pages/top', to: 'static_pages#top'
   get 'static_pages/privacy', to: 'static_pages#privacy'
