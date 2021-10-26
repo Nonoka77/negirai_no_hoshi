3f36b52545b626f13073d20cc5f7953f9581b08e
Nonoka77 <ptxdy77@gmail.com>
2021-10-26 09:55:38 +0900

    Add: likeのajax化

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,17 @@
 class LikesController < ApplicationController
+before_action :post_params, only: %i[create destroy]
+    def create
+        like = current_user.likes.create(post_id: @post.id)
+    end
+
+    def destroy
+        like = Like.find_by(post_id: @post.id, user_id: current_user.id)
+        like.destroy
+    end
+
+    private
+    
+    def post_params
+        @post = Post.find(params[:post_id])
+    end
 end
