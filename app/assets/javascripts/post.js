4c94a6bfeb585d982025a378ba6ff6af9c27f6fc
Nonoka77 <ptxdy77@gmail.com>
2021-10-27 10:20:30 +0900

    Add: update処理ををAjax化

--- app/assets/javascripts/post.js
+++ app/assets/javascripts/post.js
@@ -1,55 +1,98 @@
+$("#post_input").on("submit", function (e) {
+	e.preventDefault();
+	let inputText = $(".post_form-text").val(); // textareaの入力値を取得
+	let url = $(this).attr("action"); // action属性のurlを抽出
+	$.ajax({
+		url: url,
+		type: "POST",
+		data: {
+			post: { content: inputText },
+		},
+		dataType: "json",
+	})
+		.done(function (data) {
+			$(".posts").append(html); // 生成したHTMLをappendメソッドでドキュメントに追加
+			$(".post_form-text").val(""); // textareaを空にする
+		})
+		.fail(function () {
+			alert("error!"); // 通信に失敗した場合はアラートを表示
+		})
+		.always(function () {
+			$(".post_form-btn").prop("disabled", false); // submitボタンのdisableを解除
+			$(".post_form-btn").removeAttr("data-disable-with"); // submitボタンのdisableを解除(Rails5.0以降はこちらも必要)
+		});
+});
+$(".posts").on("click", ".post_delete", function (e) {
+	e.preventDefault();
+	e.stopPropagation();
+	let url = $(this).attr("href");
+	$.ajax({
+		url: url,
+		type: "POST",
+		data: {
+			_method: "delete",
+		},
+		dataType: "json",
+	})
+		.done(function (data) {
+			$("#post" + data.id).remove();
+		})
+		.fail(function (XMLHttpRequest) {
+			alert(XMLHttpRequest.status);
+		});
+});
 $(function () {
-	function createHTML(post) {
-		let html = `<div class="post" id="post${post.id}">
-                    <span class="post_name">${post.user.username}</span>
-                    <a class="post_delete" rel="nofollow" data-method="delete" href="/posts/${post.id}">削除</a>
-                    <p class="post_body">${post.content}</p>
-                    </div>`;
-		return html;
-	}
-	// メモ投稿(POSTメソッド)の処理
-	$("#post_input").on("submit", function (e) {
-		e.preventDefault();
-		let inputText = $(".post_form-text").val(); // textareaの入力値を取得
-		let url = $(this).attr("action"); // action属性のurlを抽出
-		$.ajax({
-			url: url,
-			type: "POST",
-			data: {
-				post: { content: inputText },
-			},
-			dataType: "json", 
-		})
-			.done(function (data) {
-				let html = createHTML(data); // 受信したデータ(data)を元に追加するURLを生成(createHTML関数は冒頭で定義)
-				$(".posts").append(html); // 生成したHTMLをappendメソッドでドキュメントに追加
-				$(".post_form-text").val(""); // textareaを空にする
-			})
-			.fail(function () {
-				alert("error!"); // 通信に失敗した場合はアラートを表示
-			})
-			.always(function () {
-				$(".post_form-btn").prop("disabled", false); // submitボタンのdisableを解除
-				$(".post_form-btn").removeAttr("data-disable-with"); // submitボタンのdisableを解除(Rails5.0以降はこちらも必要)
-			});
+	$(document).on("click",".js-edit-button", function() {
+		const postId = $(this).data('post-id');
+		const postTextArea = $('#js-textarea-post-' + postId);
+		const postButton = $('#js-post-button-' + postId);
+
+		postTextArea.show();
+		postButton.show();
 	});
-	$(".posts").on("click", ".post_delete", function (e) {
-		e.preventDefault(); 
-		e.stopPropagation(); 
-		let url = $(this).attr("href");
+});
+$(function () {
+	$(document).on("click", ".post-cancel-button", function() {
+		const postId = $(this).data('cancel-id');
+		const postTextArea = $('#js-textarea-post-' + postId);
+		const postButton = $('#js-post-button-' + postId);
+		const postError = $('#js-post-error-' + postId)
+
+		postTextArea.hide();
+		postButton.hide();
+		postError.hide();
+	});
+});
+$(function () {
+	$(document).on("click", ".post-update-button", function() {
+		const postId = $(this).data('update-id');
+		const textField = $('#js-textarea-post-' + postId);
+		const content = textField.val();
+		console.log(content);
+
 		$.ajax({
-			url: url,
-			type: "POST", 
+			url: '/posts/' + postId,
+			type: 'PATCH',
 			data: {
-				_method: "delete",
+				post: {
+					content: content
+				}
 			},
-			dataType: "json",
 		})
-			.done(function (data) {
-				$("#post" + data.id).remove(); 
-			})
-			.fail(function (XMLHttpRequest) {
-				alert(XMLHttpRequest.status);
-			});
+		.done(function (data) {
+			const postTextField = $('#post_body' + postId);
+			const postTextArea = $('#js-textarea-post-' + postId);
+			const postButton = $('#js-post-button-' + postId);
+			const postError = $('#js-post-error-' + postId);
+
+			postTextField.text(data.content);
+			postTextArea.hide();
+			postButton.hide();
+			postError.hide();
+		})
+		.fail(function () {
+			const postError = $('#js-post-error-' + postId);
+			postError.text('メッセージを入力してください')
+		})
 	});
 });
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+before_action :set_post, only: %i[edit update destroy]
 
   def create
     @post = Post.new(post_params)
@@ -10,6 +11,17 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    @post.update!(post_update_params)
+    respond_to do |f|
+      f.html { redirect_to posts_path }
+      f.json { render json: { content: @post.content, user_id: @post.user_id, id: @post.id} }
+    end 
+  end
+
+
   def index
     @post = Post.new
     @posts = Post.includes(:user)
@@ -20,7 +32,6 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @post = Post.find(params[:id])
     if @post.destroy
       respond_to do |f|
         f.html { redirect_to posts_path }
@@ -34,4 +45,12 @@ class PostsController < ApplicationController
   def post_params
     params.require(:post).permit(:content).merge(user_id: current_user.id)
   end
+
+  def post_update_params
+    params.require(:post).permit(:content)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+end
 end
